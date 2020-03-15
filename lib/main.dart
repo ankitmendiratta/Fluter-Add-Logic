82d9b6f819c3ea24ae8e6b2e927b2eeb77ebcd7c
ankit mndiratta <ankit.mendi@gmail.com>
2020-03-15 17:43:03 +0800

    Dynamiclist

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import './first_screen.dart';
 import './home.dart';
@@ -49,36 +50,53 @@ class MyFlutterApp extends StatelessWidget {
         title: 'My Flutter App',
         home: new Scaffold(
           appBar: AppBar(title: Text('My First App Screen')),
-          body: Scaffold(
-            body: getlistview(),
-          ),
+          body: getListView(),
         ));
   }
 }
 
-Widget getlistview() {
-  var list = ListView(
-    children: <Widget>[
-      ListTile(
-        title: Text('Landscape ok'),
-        subtitle: Text('beautiful landscape'),
-        leading: Icon(Icons.landscape),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint('Landscape tapped');
-        },
-      ),
-      ListTile(
-        title: Text('Windows'),
-        leading: Icon(Icons.landscape),
-      ),
-      ListTile(
-        title: Text('Phone'),
-        leading: Icon(Icons.landscape),
-      ),
-      Text('Another text')
-    ],
-  );
+List<String> getListElements() {
+  var item = List<String>.generate(50, (Counter) => 'Item $Counter');
+  return item;
+}
+
+Widget getListView() {
+  var getlistelements = getListElements();
+  var list = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      title: Text(getlistelements[index]),
+      onTap: (){
+         debugPrint('${getlistelements[index]} Tapped');
+      },
+    );
+  });
 
   return list;
 }
+
+// Widget getlistview() {
+//   var list = ListView(
+//     children: <Widget>[
+//       ListTile(
+//         title: Text('Landscape ok'),
+//         subtitle: Text('beautiful landscape'),
+//         leading: Icon(Icons.landscape),
+//         trailing: Icon(Icons.wb_sunny),
+//         onTap: () {
+//           debugPrint('Landscape tapped');
+//         },
+//       ),
+//       ListTile(
+//         title: Text('Windows'),
+//         leading: Icon(Icons.landscape),
+//       ),
+//       ListTile(
+//         title: Text('Phone'),
+//         leading: Icon(Icons.landscape),
+//       ),
+//       Text('Another text')
+//     ],
+//   );
+
+//   return list;
+// }
